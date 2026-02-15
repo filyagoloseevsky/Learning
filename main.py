8d40e8e4dbef7fc99a3714fa7940f11abe2ca6c4
filyagoloseevsky <56467436+filyagoloseevsky@users.noreply.github.com>
2026-02-15 15:11:15 +0200

    Update print statement to include 'World'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from traceback import print_tb
+import time
 
 from plyer import notification
 import time
@@ -17,10 +18,12 @@ def ceyvo():
 def reverse(text):
     return text[::-1]  #usage:  [start:stop:step]
 
+def why_not():
+    pass
 
 if __name__ == "__main__":
     print(reverse("reverse"))
 
-    print("Hello")
+    print("Hello World")
 
 
